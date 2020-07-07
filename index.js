6257e80850d82eaa37494792d3d27fafb9a551c5
mrjzhang <mrjzhang@tencent.com>
2020-07-07 10:28:17 +0800

    fix: username is required

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 /*
- * @Author: mrjzhang
+ * @Author: yiliang114
  * @Date: 2020-01-30 14:11:30
- * @LastEditors  : mrjzhang
+ * @LastEditors  : yiliang114
  * @LastEditTime : 2020-01-31 13:55:29
  */
-const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-const { getSum } = require('./lib')
+const { getSum } = require('./lib');
 
 try {
-  const username = core.getInput('username')
-  core.setOutput('username', username)
-  getSum().then(sum => {
-    core.setOutput('sum', sum)
-  })
+  const username = core.getInput('username');
+  core.setOutput('username', username);
+  getSum(username)
+    .then((sum) => {
+      core.setOutput('sum', sum);
+    })
+    .catch((error) => {
+      console.error('Error: ', error);
+    });
 } catch (error) {
-  core.setFailed(error.message)
+  core.setFailed(error.message);
 }
--- lib.js
+++ lib.js
@@ -1,46 +1,47 @@
 /*
- * @Author: mrjzhang
+ * @Author: yiliang114
  * @Date: 2020-01-30 14:19:40
- * @LastEditors  : mrjzhang
+ * @LastEditors  : yiliang114
  * @LastEditTime : 2020-01-31 12:52:41
  */
 
-let https = require('https')
-let cheerio = require('cheerio')
+let https = require('https');
+let cheerio = require('cheerio');
 //准备抓取的网站链接
-let dataUrl = 'https://github.com/yiliang114'
+let dataUrl = 'https://github.com/';
 
 //根据得到的数据，处理得到自己想要的
 function getData(str, nth = 0) {
-  const $ = cheerio.load(str)
-  const arr = $('g[transform]:last-of-type > rect:last-child')
-  const target = arr[0]
+  const $ = cheerio.load(str);
+  const arr = $('g[transform]:last-of-type > rect:last-child');
+  const target = arr[0];
   if (!arr || !arr[0]) {
-    throw new Error('can not find it')
+    throw new Error('can not find it');
   }
-  const sum = target.attribs['data-count']
-  return sum
+  const sum = target.attribs['data-count'];
+  return sum;
 }
 
-function getSum() {
+function getSum(username) {
   return new Promise((resolve, reject) => {
-    https.get(dataUrl, function(res) {
-      let str = ''
+    if (!username) reject('username is required');
+    https.get(dataUrl + username, function (res) {
+      let str = '';
       //绑定方法，获取网页数据
-      res.on('data', function(chunk) {
-        str += chunk
-      })
+      res.on('data', function (chunk) {
+        str += chunk;
+      });
       //数据获取完毕
-      res.on('end', function() {
+      res.on('end', function () {
         //调用下方的函数，得到返回值，即是我们想要的img的src
-        let data = getData(str)
-        console.log(data)
-        resolve(data)
-      })
-    })
-  })
+        let data = getData(str);
+        console.log(data);
+        resolve(data);
+      });
+    });
+  });
 }
 
 module.exports = {
-  getSum
-}
+  getSum,
+};
